799c48bd677b2b01cb7158293c13c64645b3be9d
ArialMorales <arial.morales@gmail.com>
2019-03-29 10:42:48 -0700

    Table array setters and getters
    Setters and getter for tables

--- src/com/devsmms/mindgames/game/tables/GameTable.java
+++ src/com/devsmms/mindgames/game/tables/GameTable.java
@@ -4,4 +4,12 @@ import com.devsmms.mindgames.game.pieces.Piece;
 
 public abstract class GameTable {
 	protected Piece table [][];
+
+	public Piece[][] getTable() {
+		return table;
+	}
+
+	public void setTable(Piece[][] table) {
+		this.table = table;
+	}
 }
