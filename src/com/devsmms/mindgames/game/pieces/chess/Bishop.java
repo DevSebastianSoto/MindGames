388e9e42643e12c4537d267aff6c847d3cfa4a04
ArialMorales <arial.morales@gmail.com>
2019-04-08 00:34:19 -0700

    Implemented the calcMove() method.
    Implemented the calcMove() method for the Bishop.

--- src/com/devsmms/mindgames/game/pieces/chess/Bishop.java
+++ src/com/devsmms/mindgames/game/pieces/chess/Bishop.java
@@ -14,6 +14,10 @@ public class Bishop extends Piece implements MotionPiece {
 
     @Override
     public int[][] calcMove (int coordX, int coordY) {
-        return new int[0][];
+    	int[][] answer =   {{1,1},{2,2},{3,3},{4,4},{5,5},{6,6},{7,7},
+							{1,-1},{2,-2},{3,-3},{4,-4},{5,-5},{6,-6},{7,-7},
+							{-1,1},{-2,2},{-3,3},{-4,4},{-5,5},{-6,6},{-7,7},
+							{-1,-1},{-2,-2},{-3,-3},{-4,-4},{-5,-5},{-6,-6},{-7,-7}};
+        return answer;
     }
 }
