a548b405ffe279a2176985e3cf4f4fd8484fe890
ArialMorales <arial.morales@gmail.com>
2019-04-08 00:34:05 -0700

    Implemented the calcMove() method.
    Implemented the calcMove() method for the Rook.

--- src/com/devsmms/mindgames/game/pieces/chess/Rook.java
+++ src/com/devsmms/mindgames/game/pieces/chess/Rook.java
@@ -14,6 +14,10 @@ public class Rook extends Piece implements MotionPiece {
 	
     @Override
     public int[][] calcMove (int coordX, int coordY) {
-        return new int[0][];
+    	int[][] answer =   {{0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{0,7},
+    						{0,-1},{0,-2},{0,-3},{0,-4},{0,-5},{0,-6},{0,-7},
+    						{1,0},{2,0},{3,0},{4,0},{5,0},{6,0},{7,0},
+    						{-1,0},{-2,0},{-3,0},{-4,0},{-5,0},{-6,0},{-7,0}};
+        return answer;
     }
 }
