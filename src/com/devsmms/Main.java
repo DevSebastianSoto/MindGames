408fdfea6ed45f66dcefaca48044ff7db8a71c68
ArialMorales <arial.morales@gmail.com>
2019-04-29 23:56:57 -0700

    Refactored every single chess piece suggestion.
    Refactored the suggestions for chess pieces, and highlighting them on the board in the console.

--- src/com/devsmms/Main.java
+++ src/com/devsmms/Main.java
@@ -1,13 +1,10 @@
 package com.devsmms;
 
-import com.devsmms.mindgames.ui.console.CheckersConsole;
 import com.devsmms.mindgames.ui.console.Console;
-import com.devsmms.mindgames.ui.enums.Color;
-import com.devsmms.mindgames.ui.print.ColorPrinter;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		Console.getInstance().start();
 	}
 }
